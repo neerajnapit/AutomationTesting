b05cd253e4a255bb6daef37bd2f15e4ffe3db22a
neerajnapit <78396301+neerajnapit@users.noreply.github.com>
2024-03-13 15:15:10 +0530

    Update MainSteps.java

--- src/test/java/stepDefinitions/MainSteps.java
+++ src/test/java/stepDefinitions/MainSteps.java
@@ -8,7 +8,7 @@ public class MainSteps {
 	public void user_is_on_net_banking_landing_page() {
 	    // Write code here that turns the phrase above into concrete actions
             System.out.println("User is on jenkins page and executed the github code");
-	    System.out.println("User is on netbanking landing page");
+	   // System.out.println("User is on netbanking landing page");
 	}
 	@When("User login into application")
 	public void user_login_into_application() {
